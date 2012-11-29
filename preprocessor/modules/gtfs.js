a28d6e7993ecb3ef05a5a451edf6a1dd6ba67f30
Micho <github@michael-kreil.de>
2012-11-29 15:50:09 +0100

    grobes CSV parsing läuft schon mal

--- preprocessor/modules/gtfs.js
+++ preprocessor/modules/gtfs.js
@@ -4,7 +4,7 @@ var path = require('path');
 exports.GTFS = function (foldername) {
 	var me = this;
 	
-	// Lese GTFS-Daten:
+	// Lese GTFS-Daten ... alle, außer "stop_times" ... denn die ist so fett, dass sie später geparsed wird.
 	var tables = {
 		'agency':          readCSV(foldername + '/agency.txt', true),
 		'calendar_dates':  readCSV(foldername + '/calendar_dates.txt'),
@@ -15,21 +15,38 @@ exports.GTFS = function (foldername) {
 		'frequencies':     readCSV(foldername + '/frequencies.txt'),
 		'routes':          readCSV(foldername + '/routes.txt', true),
 		'shapes':          readCSV(foldername + '/shapes.txt'),
-		'stop_times':      readCSV(foldername + '/stop_times.txt', true),
 		'stops':           readCSV(foldername + '/stops.txt', true),
 		'transfers':       readCSV(foldername + '/transfers.txt'),
 		'trips':           readCSV(foldername + '/trips.txt', true)
 	};
+	
+	// Überprüfe mal die Daten, ob alles in Ordnung ist. 
+	me.check = function () {
+		
+	}
+	
+	// Schmeiß alles weg, was nicht an den angegebenen Datumsen stattfindet
+	me.useOnly = function (dates) {
+		
+	}
+	
+	// Zum Schluss können wir erst die stop_times einlesen und alles als JSON ausgeben.
+	me.export = function (filename) {
+		//'stop_times':      readCSV(foldername + '/stop_times.txt', true),
+	}
+	
+	return me;
 }
 
 function parseCSVLine(line) {
-	// Da gibt es bestimmt noch Foo mit Kommata in Gänsefüßchen :/
+	// Da gibt es bestimmt noch Foo mit Kommata in Strings zwischen Gänsefüßchen :/
 	return line.split(',');
 }
 
 function readCSV(filename, required) {
 	filename = path.normalize(filename);
 	if (fs.existsSync(filename)) {
+		console.log('Lese "'+filename+'"');
 		
 		// Datei lesen
 		var file = fs.readFileSync(filename, 'utf-8');
@@ -39,26 +56,24 @@ function readCSV(filename, required) {
 		var lines = file.split('\r');
 		
 		// Zeilen parsen
-		for (var i = 0; i < lines.length; i++) lines[i] = parseCSVLine(lines[i]);
+		for (var i = 0; i < lines.length; i++) {
+			lines[i] = parseCSVLine(lines[i]);
+		}
 		
 		// Kopfzeile extrahieren;
 		var head = lines.shift();
 		
-		// Zeilen werden zu Objekten
+		// Zeilen als Object
 		for (var i = 0; i < lines.length; i++) {
 			var line = lines[i];
-			var object = {};
+			var obj = {};
 			for (var j = 0; j < head.length; j++) {
-				object[head[j]] = line[j];
+				obj[head[j]] = line[j];
 			}
-			lines[i] = object;
+			lines[i] = obj;
 		}
 		
-		
-		return {
-			head:head,
-			lines:lines
-		};
+		return lines;
 	} else {
 		if (required) {
 			console.error('"'+filename+'" is required but not found.')
